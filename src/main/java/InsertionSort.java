c4b2e716654f2b026640b788fbda8c5e2e8d4e78
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-11-11 14:27:03 +0300

    mission 3.4

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,23 @@
+
+
+/**
+ * Created by Dmitry on 11.11.2016.
+ */
+public class InsertionSort {
+
+    public static int[] insertSort(int[] arr) {
+        int key,temp,i,j;
+        for(i=1;i<arr.length;i++){
+            key=arr[i];
+            j=i-1;
+            while (j>=0 && key<arr[j]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                j--;
+            }
+
+        }
+        return arr;
+    }
+}
---
+++ src/test/java/InsertionSortTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dmitry on 11.11.2016.
+ */
+public class InsertionSortTest {
+
+    @Test//Positive #1
+    public void InsertionSortUnsortedList(){
+    assertArrayEquals(new int[]{0,1,2,3,4}, InsertionSort.insertSort(new int[]{4,3,2,1,0}));
+        System.out.println("Positive test has been passed '#1'");
+    }
+    @Test//Positive #2
+    public void InsertionSortSortedList(){
+        assertArrayEquals(new int[]{0,1,2,3,4}, InsertionSort.insertSort(new int[]{0,1,2,3,4}));
+        System.out.println("Positive test has been passed '#2'");
+    }
+    @Test(expected = AssertionError.class)//Negative #1
+    public void InsertionSortFromLength(){
+        assertArrayEquals(new int[]{0,1,2,3,4}, InsertionSort.insertSort(new int[]{0,1,2,3,4,5}));
+        System.out.println("Fail test has been passed '#1'");
+    }
+    @Test(expected = AssertionError.class)//Negative #2
+    public void InsertionSortWithNull(){
+        assertArrayEquals(new int[]{0,1,2,3,4}, InsertionSort.insertSort(new int[]{}));
+        System.out.println("Fail test has been passed '#1'");
+    }
+
+
+}
+/////////////////////
